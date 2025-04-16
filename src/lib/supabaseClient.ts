0a20db149ce499f7ef304b2ff6b1f4e4188172ff
mesunnyhu <mrsunnysharma99@gmail.com>
2025-04-16 18:15:08 +0530

    latest updated

--- src/lib/supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Replace with your actual Supabase project details
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
