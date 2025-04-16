4fbfeca98feae3adf454011eac831f62b8c5de81
mesunnyhu <mrsunnysharma99@gmail.com>
2025-04-16 15:05:50 +0530

    latest updated

--- src/app/api/feedback/route.ts
+++ src/app/api/feedback/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from 'next/server';
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY! // For server-side insert
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_KEY!, // For server-side insert
 );
 
 export async function POST(req: Request) {
